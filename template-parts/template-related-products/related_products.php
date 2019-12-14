5ad2e71e3ebec1b8499a30c7bb6b342602462edc
kidvan777 <haskellisp@gmail.com>
2019-12-14 13:53:50 +0300

    next

--- template-parts/template-related-products/related_products.php
+++ template-parts/template-related-products/related_products.php
@@ -1,82 +1,82 @@
-<!-- RELATED PRODUCTS -->
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="top_hits_heading flex space_between">
-                    <div class="top_hits_head">
-                        TOP HITS
+<!-- SECTION TOP HITS -->
+    <section class="top_hits_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="top_hits_heading flex space_between">
+                        <div class="top_hits_head">
+                            TOP HITS
+                        </div>
+                        <div class="top_hits_variation">
+                            <ul>
+                                <li>Lamps</li>
+                                <li>Bulbs</li>
+                                <li>Office</li>
+                                <li class="top_hits_arrow"><img src="<?php echo get_template_directory_uri() . '/assets/img/keyboard_arrow_left-24px.svg'?>" alt=""></li>
+                                <li class="top_hits_arrow"><img src="<?php echo get_template_directory_uri() . '/assets/img/keyboard_arrow_right-24px.svg'?>" alt=""></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="top_hits_variation">
-                        <ul>
-                            <li>Lamps</li>
-                            <li>Bulbs</li>
-                            <li>Office</li>
-                            <li><img src="<?php echo get_template_directory_uri() . '/assets/img/keyboard_arrow_left-24px.svg'?>" alt=""></li>
-                            <li><img src="<?php echo get_template_directory_uri() . '/assets/img/keyboard_arrow_right-24px.svg'?>" alt=""></li>
-                        </ul>
+                    <div class="top_hits_line">
                     </div>
-                </div>
-                <div class="top_hits_line">
-                </div>
-                <div class="top_hits flex">
-                    <div class="top_hits_items col-lg-2 col-md-4">
-                        <img src="<?php echo get_template_directory_uri() . '/assets/img/best_2.jpg'?>" alt="">
+                    <div class="top_hits flex">
+                        <div class="top_hits_item top_hits_item_banner col-lg-2 col-md-4 col-sm-6">
+                            <img src="<?php echo get_template_directory_uri() . '/assets/img/best_2.jpg'?>" alt="">
+
+                        </div>
+                        <div class="top_hits_item col-lg-2 col-md-4 col-sm-6">
+                            <div class="top_hits_item__img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/asd_bulb_7.jpg'?>" alt="">
+                            </div>
+                            <div class="top_hits_item__span">
+                                <span>
+                                    Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
+                                </span>
+                            </div>
+                        </div>
+                        <div class="top_hits_item col-lg-2 col-md-4 col-sm-6">
+                            <div class="top_hits_item__img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb1.png'?>" alt="">
+                            </div>
+                            <div class="top_hits_item__span">
+                                <span>
+                                    Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
+                                </span>
+                            </div>
+                        </div>
+                        <div class="top_hits_item col-lg-2 col-md-4 col-sm-6">
+                            <div class="top_hits_item__img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/asd_bulb_6.png'?>" alt="">
+                            </div>
+                            <div class="top_hits_item__span">
+                                <span>
+                                    Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
+                                </span>
+                            </div>
+                        </div>
+                        <div class="top_hits_item col-lg-2 col-md-4 col-sm-6">
+                            <div class="top_hits_item__img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb2.png'?>" alt="">
+                            </div>
+                            <div class="top_hits_item__span">
+                                <span>
+                                    Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
+                                </span>
+                            </div>
+                        </div>
+                        <div class="top_hits_item col-lg-2 col-md-4 col-sm-6">
+                            <div class="top_hits_item__img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb1.png'?>" alt="">
+                            </div>
+                            <div class="top_hits_item__span">
+                                <span>
+                                    Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
+                                </span>
+                            </div>
+                        </div>
 
                     </div>
-                    <div class="top_hits_item col-lg-2 col-md-4">
-                        <div class="top_hits_item__img">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/asd_bulb_7.jpg'?>" alt="">
-                        </div>
-                        <div>
-                            <span class="top_hits_item__span">
-                                Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
-                            </span>
-                        </div>
-                    </div>
-                    <div class="top_hits_item col-lg-2 col-md-4">
-                        <div class="top_hits_item__img">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb1.png'?>" alt="">
-                        </div>
-                        <div>
-                            <span class="top_hits_item__span">
-                                Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
-                            </span>
-                        </div>
-                    </div>
-                    <div class="top_hits_item col-lg-2 col-md-4">
-                        <div class="top_hits_item__img">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/asd_bulb_6.png'?>" alt="">
-                        </div>
-                        <div>
-                            <span class="top_hits_item__span">
-                                Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
-                            </span>
-                        </div>
-                    </div>
-                    <div class="top_hits_item col-lg-2 col-md-4">
-                        <div class="top_hits_item__img">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb2.png'?>" alt="">
-                        </div>
-                        <div>
-                            <span class="top_hits_item__span">
-                                Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
-                            </span>
-                        </div>
-                    </div>
-                    <div class="top_hits_item col-lg-2 col-md-4">
-                        <div class="top_hits_item__img">
-                            <img src="<?php echo get_template_directory_uri() . '/assets/img/bulb1.png'?>" alt="">
-                        </div>
-                        <div>
-                            <span class="top_hits_item__span">
-                                Лампа светодиодная LED-A60-standard 11Вт 160-260В Е27 3000К 990Лм ASD
-                            </span>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
